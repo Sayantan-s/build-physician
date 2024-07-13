b5e1a242fa6c8b16eeede4e8ac30b21613c78cb9
Sayantan Samanta <sssamanta789@gmail.com>
2024-07-13 10:19:26 +0530

    refactor(schema): changed user schema

--- api/db/models/user.py
+++ api/db/models/user.py
@@ -1,14 +1,12 @@
-from peewee import CharField,UUIDField, TextField, DateTimeField
+from peewee import CharField,TextField, DateTimeField
 from . import BaseModel
 from datetime import datetime
-import uuid
-
-
 class User(BaseModel):
-    id = UUIDField(primary_key=True, default=uuid.uuid4)
-    first_name = TextField(column_name='first_name')
-    last_name = TextField(column_name='last_name')
+    id = CharField(primary_key=True)
+    name = TextField(column_name='name')
     email = CharField(column_name='email')
+    picture = TextField(column_name='profile_pic')
+    new_user = TextField(column_name='new_user')
     created_at = DateTimeField(default=datetime.utcnow, column_name='created_at')
     updated_at = DateTimeField(default=datetime.utcnow, column_name='updated_at')
 
