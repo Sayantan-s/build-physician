84497bd78318f046dc62838c8994adcbe10a2a6f
Sayantan <sayantan@hyresnap.com>
2025-03-11 02:22:00 +0530

    feat: added py environemnt

--- api/integrations/firebase.py
+++ api/integrations/firebase.py
@@ -1,6 +1,11 @@
+import os
+
 from firebase_admin import credentials, initialize_app
 
 class Firebase:
     def __init__(self) -> None:
+        env = os.getenv('PY_ENV')
+        if env == 'prod':
+            return
         cred = credentials.Certificate("firebase-adminsdk-o3fku-a740a9dc1d.json")
         initialize_app(cred)
